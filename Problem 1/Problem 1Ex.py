a83de003d2c8400c29e856e27cf7d2525cbb4c67
Puhandsome <148200765+Puhandsome@users.noreply.github.com>
2023-10-17 16:40:31 +0700

    Delete Problem 1 directory

--- Problem 1/Problem 1Ex.py
+++
@@ -1,6 +0,0 @@
-m = int(input())
-n = int(input())
-if 1<=m<=20 and 1<=n<=20:
-    print(((("*"*m)+"\n"))*n)
-else:
-    print("Invalid input")
--- Problem 1/ggg.py
+++
@@ -1,6 +0,0 @@
-x = input("Enter an integer: ")
-y = input("Enter another integer: ")
-x = int(x)
-y = int(y)
-result = x*y
-print(result)
